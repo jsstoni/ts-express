660d2f047b50afd368f4d0e2100cc4117ec3aa41
jsstoni <jsstoniha@gmail.com>
2023-10-16 21:17:58 -0300

    example error

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -5,4 +5,12 @@ router.get("/", (_req: Request, res: Response, _next: NextFunction) => {
     res.status(200).send("hola mundo");
 });
 
+router.get("/error", (_req: Request, _res: Response, next: NextFunction) => {
+    try {
+        throw new Error("Error");
+    } catch {
+        next("My custom Error");
+    }
+});
+
 export default router;
